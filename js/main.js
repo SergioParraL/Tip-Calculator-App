bf0fd0aae8bc1af29038b2d1c2c1274f9597a42a
Sergio Parra <sergiolparra666@gmail.com>
2021-08-19 23:36:20 -0430

    Add: Calculaion logic | Form control | Error message

--- js/main.js
+++ js/main.js
@@ -1,34 +1,82 @@
 // ( bill * percentage ) / Number People == tip mount
 // ((bill * percentage ) + bill) / numberPeople == total 
-let $button = document.querySelector('button')
-
-$button.addEventListener('click',((e)=>{
-	e.preventDefault()
-}))
-
-let $bill = document.querySelector('#bill')
-let $numberPeople = document.querySelector('#people')
-let $percentage = document.querySelectorAll('.percentage')
-
-function calculator (bill,pertg,peopleN) {
-	this.bill = bill
-	this.pertg = pertg
-	this.peopleN = peopleN
-	
-	let base = this.bill * this.percentage
-	
-	this.tipMount = (() =>{ // return the tip that each one must give 
-		return base  / this.peopleN
-	})
-	this.total = (() => { // return the amount total to pay
-		return (base + this.bill ) / this.peopleN 
-	})
+const $button = document.querySelectorAll('button')
+const $input = document.querySelectorAll('input[type=text]')
+let $countOfData = 0 
+const $errorMessage = `Can't be zero` 
+let $count = {
+	bill : 0,
+	personNum : 0,
+	percent : 0,
+	tipMount : function () {
+		return ((this.bill * this.percent) / 100 ) / this.personNum
+	},
+	total : function (){
+		return (((this.bill * this.percent) / 100 ) + this.bill) / this.personNum
+	}
 }
 
+$button.forEach( function(e,i) {
+	e.addEventListener('click',((e)=>{
+		e.preventDefault()
+		let q = parseInt(e.target.name)
+		$count.percent = q
+		$countOfData++
+		// gap(q)
+	}))
+});
 
-$bill.addEventListener('keydown',input)
-$numberPeople.addEventListener('keydown',input)
+$input.forEach( function(e) {
+	e.addEventListener('blur',input)
+});
 
-function input (e) {
-	console.log(arguments)
+function input () {
+	const attr = this.getAttribute('name')
+	const float = parseFloat(this.value)
+	if (float > 0 && float != NaN) {
+ 		if( attr == 'bill'){
+			console.log('¡Tío bill eres tú!')
+			$count.bill = float
+			$countOfData++ 
+		}
+		if (attr == 'people') {
+			console.log('people')
+			$count.personNum = float
+			$countOfData++ 
+			
+		}
+		if (attr == 'custom') {
+			console.log('custom')
+			$count.percent = float
+			$countOfData++ 
+		}
+		if ($countOfData == 3){
+			paint()
+		}
+	}else {
+		console.log(this)
+		const span = document.createElement('span')
+		const text = document.createTextNode($errorMessage)
+		span.appendChild(text)
+		span.id = 'span'
+		this.parentNode.appendChild(span)
+		setTimeout((() => {
+			const deleteNode = document.querySelector('#span')
+			deleteNode.parentNode.removeChild(deleteNode)
+		}),5000)
+		// this.parentNode.innerHTML = "<p>$errorMessage</p>"
+		// console.log($errorMessage)
+	}
+}
+
+function paint () {
+	$countOfData = 0
+	let objTotal = $count.total()
+	let objTip = $count.tipMount()
+
+	let tip = document.querySelector('#tip_Amount')
+	let total = document.querySelector('#total')
+
+	tip.innerHTML = objTip.toFixed(2)
+	total.innerHTML = objTotal.toFixed(2)
 }
